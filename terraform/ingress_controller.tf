a3510537f1fda780fd10c847a3fefc9cca0231e7
Ayomide Joshua Fabiyi <fbayomide@gmail.com>
2023-07-09 17:54:11 +0100

    - updated terraform workflow to install helm

--- terraform/ingress_controller.tf
+++ terraform/ingress_controller.tf
@@ -1,13 +1,13 @@
 
-resource "helm_release" "nginx-ingress-controller" {
-  name       = "nginx-ingress-controller"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "nginx-ingress-controller"
+# resource "helm_release" "nginx-ingress-controller" {
+#   name       = "nginx-ingress-controller"
+#   repository = "https://charts.bitnami.com/bitnami"
+#   chart      = "nginx-ingress-controller"
 
 
-  set {
-    name  = "service.type"
-    value = "LoadBalancer"
-  }
+#   set {
+#     name  = "service.type"
+#     value = "LoadBalancer"
+#   }
 
-}
+# }
